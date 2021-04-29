0c1c1d7784f6082efe4b4896a4f80f7404a77433
Karen <devkarenc@gmail.com>
2021-04-29 15:22:52 -0400

    Create and sync models using Sequelize

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const morgan = require("morgan");
 const path = require("path");
 const app = express();
 const layout = require("./layout");
-const { db } = require("./models");
+const { db, Page, User } = require("./models");
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+
+// Page.sync({ force: true }).then(() => {
+//   return Page.create({
+//     title: "Page 1",
+//     slug: "test",
+//     content: "test",
+//     status: "open",
+//   });
+// });
+
+// User.sync({ force: true }).then(() => {
+//   return User.create({
+//     name: "test",
+//     email: "test@gmail.com",
+//   });
+// });
 
 db.authenticate().then(() => {
   console.log("connected to the database");
@@ -18,6 +34,12 @@ app.get("/", (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+
+const init = async () => {
+  await db.sync({ force: true });
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
+};
+
+init();
--- models/index.js
+++ models/index.js
@@ -1,4 +1,31 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/wikistack");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false,
+});
 
-module.exports = { db };
+const Page = db.define("page", {
+  title: {
+    type: Sequelize.STRING,
+  },
+  slug: {
+    type: Sequelize.STRING,
+  },
+  content: {
+    type: Sequelize.TEXT,
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed"),
+  },
+});
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+    isEmail: true,
+  },
+});
+
+module.exports = { db, Page, User };
